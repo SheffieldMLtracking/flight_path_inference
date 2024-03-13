32f0669f1152f17fabec8c621ef8e0413ca27a4e
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-03-13 15:57:10 +0000

    Start writing a test

--- tests/test_trajectory_gp.py
+++ tests/test_trajectory_gp.py
@@ -2,4 +2,16 @@ import flight_path_inference.trajectory_gp
 
 
 def test_trajectory_gp():
-    flight_path_inference.trajectory_gp.run()
+    """
+    Test the gaussian process flight inference.
+
+    Based on:
+    https://github.com/lionfish0/beelabel/blob/master/labelling/compute5.ipynb
+    """
+    # Z,mu,scale = tgp.run(obstimes,observations,500,learning_rate=0.1,likenoisescale=0.05,Nind=15)
+    obstimes = (1710345107.0, 1710345108.0, 1710345109.0)
+    observations = (
+        # (originX, originY, originZ, vectorX, vectorY, vectorZ, confidence)
+        # TODO (0.,0.,0., ???)
+    )
+    flight_path_inference.trajectory_gp.run(obstimes, observations)
